d92a6b2ed60c04955f580d9d3c3980ca2b8c7ba7
Mir Rahed Uddin <rahedmir@gmail.com>
2021-09-03 20:07:57 +0530

    Add files via upload

---
+++ VS 2008/VB Browser/VB Browser/frmSource.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSource
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSource))
+        Me.txtSource = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'txtSource
+        '
+        Me.txtSource.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtSource.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSource.Location = New System.Drawing.Point(0, 0)
+        Me.txtSource.Multiline = True
+        Me.txtSource.Name = "txtSource"
+        Me.txtSource.ReadOnly = True
+        Me.txtSource.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtSource.Size = New System.Drawing.Size(801, 441)
+        Me.txtSource.TabIndex = 0
+        Me.txtSource.TabStop = False
+        Me.txtSource.WordWrap = False
+        '
+        'frmSource
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(801, 441)
+        Me.Controls.Add(Me.txtSource)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmSource"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Source"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtSource As System.Windows.Forms.TextBox
+End Class
